5e8967d531fa658ede08f9f2430c29efaea66404
Konstantin S. Uvarin <khedin@gmail.com>
2017-10-22 02:18:57 +0300

    TST Fix examples to match changed API

--- example/max_time.pl
+++ example/max_time.pl
@@ -6,13 +6,16 @@ use Time::HiRes qw(time sleep);
 
 use Benchmark::Linear;
 
-my $bl = Benchmark::Linear->new( code => sub { sleep $_/100 }, cleanup => sub { print "Done $_\n" } );
+my $bl = Benchmark::Linear->new(
+    code => sub { sleep $_/100 },
+    cleanup => sub { print "Done sleep $_/100\n" },
+);
 
 my $t0 = time;
 $bl->run( max_time => 1 );
 my $time = time - $t0;
 
-print "runtime = $time\n";
-print "time per step in sleep = ".$bl->time_per_op;
+print "Total elapsed time = $time\n";
+print "time per step in sleep = ".$bl->get_approx->linear;
 
 
--- example/short.pl
+++ example/short.pl
@@ -5,8 +5,11 @@ use warnings;
 use Time::HiRes qw(sleep);
 use Benchmark::Linear qw(bench);
 
+print "'Benchmarking' a sleep( n/100 )\n";
+
 my $bl = bench {
-    sleep $_ / 100;
+    warn "Arg: $_";
+    sleep( $_ / 100 );
 };
 
-printf "Avg sleep time: %f\n", $bl->time_per_op;
+printf "Operations per second: %f\n", $bl->ops_per_sec;
